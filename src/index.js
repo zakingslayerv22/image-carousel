f428888d6541acca092f64909dad56c6993fecb0
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-11-10 05:56:24 -0800

    Import and append images
    Import the images. Select
    the picture-frame and
    the
    slide-images-container.
    
    Create new Image
    instances for the three
    images, give the Image
    instances , the
    respective sources
    and classes.
    
    Append the three images to
    the slide-images-container.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./index.css";
+import ImageOne from "./images/1.png";
+import ImageTwo from "./images/2.png";
+import ImageThree from "./images/3.png";
+
+const pictureFrame = document.querySelector(".picture-frame");
+const slideImagesContainer = document.querySelector(".slide-images-container");
+
+const imageOne = new Image();
+imageOne.src = ImageOne;
+imageOne.classList.add("image-one", "slide");
+
+const imageTwo = new Image();
+imageTwo.src = ImageTwo;
+imageTwo.classList.add("image-two", "slide");
+
+const imageThree = new Image();
+imageThree.src = ImageThree;
+imageThree.classList.add("image-three", "slide");
+
+slideImagesContainer.append(imageOne, imageTwo, imageThree);
