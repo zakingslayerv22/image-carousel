4f9185edf7d4dce8133e3753c9eae23e919fa807
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-11-11 03:14:02 -0800

    Handle next button click
    In the constructor, initialize
    an counter index property and set
    it to 1.
    
    Initialize a nextSlide method()
    that checks if index is
    less than the length of the
    images array (this is because
    the first image is already
    displayed onload and since index
    starts at 1, it should display
    elements and 1 and 2, supposing
    there are 3 images in total),
    it clears the picture frame,
    appends the element that
    corresponds to the index of
    this.index in the imagesArray
    and increments this.index.
    
    Initialize a handleClicks()
    method that selects the
    next button and attaches a
    click event listener to it.
    When the next button is clicked,
    the nextSlide() method is called.
    
    Call handleClicks() in intialize()
    so that when the class initializes,
    the event listener attached to
    the next button is set.
    
    This setup displays the next
    image on click of the next button.

--- src/image-carousel-module/image-carousel-module.js
+++ src/image-carousel-module/image-carousel-module.js
@@ -3,11 +3,28 @@ export class ImageCarousel {
     this.allImages = document.querySelectorAll(".slide");
     this.imagesArray = [...this.allImages];
     this.pictureFrame = document.querySelector(".picture-frame");
+    this.index = 1;
 
     if (this.imagesArray.length > 1) this.initialize();
   }
 
   initialize() {
     this.pictureFrame.append(this.imagesArray[0]);
+    this.handleClicks();
+  }
+
+  nextSlide() {
+    if (this.index < this.imagesArray.length) {
+      this.pictureFrame.textContent = "";
+      this.pictureFrame.append(this.imagesArray[this.index]);
+      this.index++;
+    }
+  }
+
+  handleClicks() {
+    const nextButton = document.querySelector(".next-button");
+    nextButton.addEventListener("click", () => {
+      this.nextSlide();
+    });
   }
 }
