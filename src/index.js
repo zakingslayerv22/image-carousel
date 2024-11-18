076b4535c29e0932de17d3c15ad2d84d8d65772b
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-11-18 09:19:30 -0800

    Append the images
    In the index.js,
    select the individual divs
    that is meant to hold
    and append the images to
    their respective divs.
    
    Create new divs that will
    hold the image captions,
    give them respective
    classnames and textContent.
    Append them to the
    respective divs.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,42 @@ import ImageTwo from "./images/2.png";
 import ImageThree from "./images/3.png";
 import { ImageCarousel } from "./image-carousel-module/image-carousel-module";
 
-const pictureFrame = document.querySelector(".picture-frame");
-const slideImagesContainer = document.querySelector(".slide-images-container");
+const imageOneContainer = document.querySelector(".number-one");
 
 const imageOne = new Image();
 imageOne.src = ImageOne;
 imageOne.classList.add("image-one", "slide");
 
+const imageOneCaption = document.createElement("div");
+imageOneCaption.classList.add("image-caption");
+imageOneCaption.textContent = "Image One";
+
+imageOneContainer.append(imageOne, imageOneCaption);
+
+const imageTwoContainer = document.querySelector(".number-two");
+
 const imageTwo = new Image();
 imageTwo.src = ImageTwo;
 imageTwo.classList.add("image-two", "slide");
+
+const imageTwoCaption = document.createElement("div");
+imageTwoCaption.classList.add("image-caption");
+imageTwoCaption.textContent = "Image Two";
+
+imageTwoContainer.append(imageTwo, imageTwoCaption);
+
+const imageThreeContainer = document.querySelector(".number-three");
 
 const imageThree = new Image();
 imageThree.src = ImageThree;
 imageThree.classList.add("image-three", "slide");
 
-slideImagesContainer.append(imageOne, imageTwo, imageThree);
+const imageThreeCaption = document.createElement("div");
+imageThreeCaption.classList.add("image-caption");
+imageThreeCaption.textContent = "Image Three";
+
+imageThreeContainer.append(imageThree, imageThreeCaption);
 
 window.onload = () => {
-  new ImageCarousel();
+  // new ImageCarousel();
 };
