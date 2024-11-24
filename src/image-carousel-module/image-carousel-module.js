56c89737a2573be2d94750f0f661e56420db0f9e
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2024-11-24 05:31:06 -0800

    Implement dot navigation
    First use the spread operator
    to unpack properties from the
    allDots nodelist.
    
    Define a method jumpToSlide
    that takes in
    a slideIndex as an arg.
    It loops through all the slides
    and transfroms each of them
    to the set of values that will
    bring the slide, whose
    index(in allSlides) was
    passed into it, to the viewport.
    
    Consider this:
    
    Slide Two, has an index of 1 in
    allSlides and requires
    transfrom values of
    
    `Slide 1 : translate(-100%)`
    `Slide 2: translate(-100%)`
    `Slide 2: translate(-100%)`
    
    to be visible in the viewport.
    Assign the value of slideIndex
    to currentSlideIndex so that
    the currentSlideIndex stays
    ready for nextButton and
    previousButton clicks after
    jumpToSlide.
    
    In handleClicks, add click
    event listeners to each
    dot and call the jumpToSlide
    with the respective arg
    for each.
    
    With this setup, clicking
    on any dot navigates to
    the respective slide.

--- src/image-carousel-module/image-carousel-module.js
+++ src/image-carousel-module/image-carousel-module.js
@@ -2,6 +2,12 @@ export class ImageCarousel {
   constructor() {
     this.allSlides = document.querySelectorAll(".slide");
     this.allDots = document.querySelectorAll(".dot");
+
+    [this.dotOne, this.dotTwo, this.dotThree] = [...this.allDots];
+
+    // this.dotOne = document.querySelector(".dot-one");
+    // this.dotTwo = document.querySelector(".dot-two");
+    // this.dotThree = document.querySelector(".dot-three");
     this.currentSlideIndex = 0;
 
     if (this.allSlides.length > 1) this.initialize();
@@ -21,6 +27,18 @@ export class ImageCarousel {
 
     previousButton.addEventListener("click", () => {
       this.previousSlide();
+    });
+
+    this.dotOne.addEventListener("click", () => {
+      this.jumpToSlide(0);
+    });
+
+    this.dotTwo.addEventListener("click", () => {
+      this.jumpToSlide(1);
+    });
+
+    this.dotThree.addEventListener("click", () => {
+      this.jumpToSlide(2);
     });
   }
 
@@ -71,5 +89,13 @@ export class ImageCarousel {
 
   removeActiveClassFromAllDots() {
     this.allDots.forEach((dot) => dot.classList.remove("active"));
+  }
+
+  jumpToSlide(slideIndex) {
+    this.allSlides.forEach((slide) => {
+      slide.style.transform = `translate(${slideIndex * -100}%)`;
+    });
+
+    this.currentSlideIndex = slideIndex;
   }
 }
